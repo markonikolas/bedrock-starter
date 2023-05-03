a87704c7ff056eb5501eb9ad2cd5420c526e058a
Marko Nikolaš <marko.nikolas@protonmail.com>
2023-05-03 13:34:13 +0200

    Update bud / lando configs
    Resolved issues with bud that prevented hmr from working.

--- web/app/themes/sage/bud.config.js
+++ web/app/themes/sage/bud.config.js
@@ -13,73 +13,32 @@ export default async (app) => {
    * @see {@link https://bud.js.org/docs/bud.entry/}
    */
   app
-    .entry({
-      app: ['@scripts/app', '@styles/app'],
-      editor: ['@scripts/editor', '@styles/editor'],
-    })
-
-    /**
-     * Directory contents to be included in the compilation
-     * @see {@link https://bud.js.org/docs/bud.assets/}
-     */
+    .entry('app', ['@scripts/app', '@styles/app'])
+    .entry('editor', ['@scripts/editor', '@styles/editor'])
     .assets(['images'])
 
     /**
-     * Matched files trigger a page reload when modified
-     * @see {@link https://bud.js.org/docs/bud.watch/}
+     * Set public path
+     *
+     * @see {@link https://bud.js.org/docs/bud.setPublicPath}
      */
+    .setPublicPath('/app/themes/sage/public/');
+
+  /**
+   * Development server settings
+   *
+   * @see {@link https://bud.js.org/docs/bud.setUrl}
+   * @see {@link https://bud.js.org/docs/bud.setProxyUrl}
+   * @see {@link https://bud.js.org/docs/bud.watch}
+   */
+  app
     .watch([
+      'resources/views',
+      'app',
       'tailwind.config.js',
-      'resources/views/**/*.blade.php',
-      'app/View/**/*.php',
+      'index.php',
+      'functions.php',
     ])
-
-    /**
-     * URI of the `public` directory
-     * @see {@link https://bud.js.org/docs/bud.setPublicPath/}
-     */
-    .setPublicPath('/app/themes/sage/public/')
-
-    /**
-     * Development origin
-     */
-    .serve('http://bedrock.lndo.site')
-
-    /**
-     * Generate WordPress `theme.json`
-     *
-     * @note This overwrites `theme.json` on every build.
-     *
-     * @see {@link https://bud.js.org/extensions/sage/theme.json/}
-     * @see {@link https://developer.wordpress.org/block-editor/how-to-guides/themes/theme-json/}
-     */
-    .wpjson.settings({
-      color: {
-        custom: false,
-        customDuotone: false,
-        customGradient: false,
-        defaultDuotone: false,
-        defaultGradients: false,
-        defaultPalette: false,
-        duotone: [],
-      },
-      custom: {
-        spacing: {},
-        typography: {
-          'font-size': {},
-          'line-height': {},
-        },
-      },
-      spacing: {
-        padding: true,
-        units: ['px', '%', 'em', 'rem', 'vw', 'vh'],
-      },
-      typography: {
-        customFontSize: false,
-      },
-    })
-    .useTailwindColors()
-    .useTailwindFontFamily()
-    .useTailwindFontSize()
-    .enable();
+    .proxy('http://bedrock.lndo.site')
+    .serve('http://0.0.0.0:3000');
 };
--- web/app/themes/sage/resources/scripts/app.js
+++ web/app/themes/sage/resources/scripts/app.js
@@ -4,7 +4,7 @@ import domReady from '@roots/sage/client/dom-ready';
  * Application entrypoint
  */
 domReady(async () => {
-  // ...
+  console.log('Hello from bud!');
 });
 
 /**
