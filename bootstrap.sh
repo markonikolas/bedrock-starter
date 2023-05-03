e765d74457666b3729eba16b42382656f1c61c67
Marko Nikolaš <marko.nikolas@protonmail.com>
2023-05-03 19:56:47 +0200

    Update install process

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,7 @@ cp .env.example .env
 
 lando start && \
     unzip demo.sql.zip && \
+    lando wp db create && \
     lando db-import demo.sql && \
     lando composer install && \
     cd web/app/themes/sage && \
