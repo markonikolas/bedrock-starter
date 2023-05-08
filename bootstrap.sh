935e2996930f308c68e320718c876a01c50d7d31
Marko Nikolaš <marko.nikolas@protonmail.com>
2023-05-08 20:44:30 +0200

    Update development environment

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,30 +1,25 @@
-lando start
-
-sleep 1
-
 echo 'Installing project dependencies, please wait...' && \
+    lando start && \
     lando composer install ;
 
 echo 'Installing theme dependencies, please wait...' && \
     cd web/app/themes/sage && \
     lando composer install && \
     lando yarn && \
-    lando yarn build
-
-sleep 1
+    lando yarn build ;
 
 echo 'Creating a demo database, please wait...' && \
     cd ../../../../ && \
-    lando wp db check && \
-    lando wp db create ;
+    lando wp db check ;
 
-sleep 1
+echo 'Importind demo database, please wait...' && \
+    lando db-import demo.* ;
 
-lando db-import demo.zip
+echo 'Checking the database, please wait...' && \
+    lando wp db check ;
 
-sleep 1
-
-lando wp db check
+echo 'Restarting containers, please wait...' && \
+    lando restart ;
 
 echo '---------------'
 echo '               '
